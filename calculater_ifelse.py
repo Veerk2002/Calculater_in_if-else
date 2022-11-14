1677c03b20624c3b98071adf4f33c8180a2b2370
veerkaur321 <118209190+veerkaur321@users.noreply.github.com>
2022-11-15 04:30:49 +0530

    Add files via upload

---
+++ calculater_ifelse.py
@@ -0,0 +1,41 @@
+#create a simple calculater in if else?
+
+#pending see video from youtube and complete it?
+#CALCULATER!!!
+
+# num=int(input("enter the first number: "))
+# num1=int(input("enter the second number: "))
+# print("+,-,*,%,/,//,^")
+# opr=(input("enter the oprator: "))
+# if opr=="+":
+#     print(num+num1)
+# elif opr=="-":
+#        print(num-num1)
+# elif opr=="*":
+#         print(num*num1)
+# elif opr=="%":
+#     print(num%num1)
+# elif opr=="/":
+#         print(num/num1)
+# elif opr=="//":
+#     print(num//num1)
+# elif opr=="^":
+#     print(num^num1)
+# else:
+#     print("invalid operator")
+
+# a=["mango","apple"]
+# print(id(a))
+# b=["mango","apple"]
+# print(id(b))
+# x=a
+# z=b
+# print(id(x))
+# # print(a is x)
+# print(b is z)
+
+# a=8
+# b='fghj'
+# print(a+b)
+
+
